45c8dc2a2563ca8fdaca48c6a7ec5b1ea3b8f8dc
Malachi Bowman <malachiabowman@gmail.com>
2020-07-31 21:35:15 -0500

    added confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_012005) do
+ActiveRecord::Schema.define(version: 2020_08_01_022704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -136,6 +136,10 @@ ActiveRecord::Schema.define(version: 2020_08_01_012005) do
     t.string "card_exp_year"
     t.text "extra_billing_info"
     t.datetime "read_notification"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
